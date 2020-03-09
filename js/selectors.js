2cbe51e896cf9ba6e0a7c07831e11303aaed221c
James Stamps Jr <jstampshere@gmail.com>
2020-03-09 23:02:39 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,18 @@
 // declare your functions here...
+
+function paragraphSelector() {
+ return ($('p'));
+}
+
+function lastImageSelector() {
+  return ($('img').last());
+}
+
+
+
+//	$( "li" ).last().css( "background-color", "red" );
+
+
+/*  $(document).ready(function(){
+  $("div").last();
+});  */
